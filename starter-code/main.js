86d84641dc77fd3c885eb9f4ba32cb2e0eda74dc
Brad Volts <Brad@Brads-MacBook-Pro.local>
2016-08-05 18:45:46 -0700

    equality check

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,10 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 console.log("JS file is connected to HTML! Woo!")
+if (cardOne === cardTwo) {
+	alert("You found a match!")
+} else if (cardThree === cardFour) {
+	alert("You found a match!")
+} else {
+	alert("Sorry, try again.")
+}
