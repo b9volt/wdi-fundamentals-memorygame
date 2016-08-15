2752e5067b442320d83e9167ada8decbcf976a2a
Brad Volts <Brad@Brads-MacBook-Pro.local>
2016-08-15 16:13:37 -0700

    unit 10 commit

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,11 +2,21 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
-console.log("JS file is connected to HTML! Woo!")
-if (cardOne === cardTwo) {
+
+/* if (cardOne === cardTwo) {
 	alert("You found a match!")
 } else if (cardThree === cardFour) {
 	alert("You found a match!")
 } else {
 	alert("Sorry, try again.")
+} */
+
+var board = document.getElementById('game-board');
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('div');
+		cardElement.className = 'card';
+
+		board.appendChild(cardElement);	
+	}	
 }
